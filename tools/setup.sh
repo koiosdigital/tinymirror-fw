9f0a31e4d901013ab6bb9bc67312577bb0bf49cc
Aiden Vigue <aiden@vigue.me>
2024-07-05 22:56:53 -0400

    test

--- tools/setup.sh
+++ tools/setup.sh
@@ -6,4 +6,4 @@
 apt-get update -qq
 
 apt-get install -qq -y gawk wget git-core diffstat unzip texinfo gcc-multilib \
-    build-essential chrpath socat libsdl1.2-dev xterm
+    build-essential chrpath socat libsdl1.2-dev xterm cowsay
