bee83e9d0afef1ac3d01362958239b85996161c8
Aiden Vigue <aiden@vigue.me>
2024-07-06 16:56:12 -0400

    remove travis

--- tools/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# setup.sh: Poku build setup
-# Builds at: https://travis-ci.org/vlvrd/yocto-build
-
-# Install Yocto dependencies
-apt-get update -qq
-
-apt-get install -qq -y gawk wget git-core diffstat unzip texinfo gcc-multilib \
-    build-essential chrpath socat libsdl1.2-dev xterm cowsay
